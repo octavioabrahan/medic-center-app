5d00175ed758aeffa9a7d2fac483b2c7ec73f26d
Octavio <octavio1709@gmail.com>
2025-04-03 17:11:21 -0300

    modified:   backend/routes/horarios.js

--- backend/routes/horarios.js
+++ backend/routes/horarios.js
@@ -2,10 +2,14 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/horariosController');
 
+
 // GET /api/horarios/medico/:id_medico -> listado completo
 router.get('/medico/:id_medico', controller.getHorariosPorMedico);
 
 // GET /api/horarios/disponibles/:id_medico -> solo fechas disponibles
 router.get('/disponibles/:id_medico', controller.getFechasDisponibles);
+
+// POST para horarios
+router.post('/', controller.crearHorario);
 
 module.exports = router;
