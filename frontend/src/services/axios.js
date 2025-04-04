87d8335a328c5d38df73f8c4c2ac42b2327bdadd
Octavio <octavio1709@gmail.com>
2025-04-04 18:59:48 -0300

    modified:   frontend/.env 	modified:   frontend/src/services/axios.js

--- frontend/src/services/axios.js
+++ frontend/src/services/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:3002',
+  baseURL: process.env.REACT_APP_API_URL || 'http://10.20.20.111:3002',
 });
 
 export default instance;
