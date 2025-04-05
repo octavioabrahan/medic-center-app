e0d19529c119f9f2ee641ae92fa6fe3dacbe6604
Octavio <octavio1709@gmail.com>
2025-04-04 21:31:49 -0300

    modified:   backend/models/db.js

--- backend/models/db.js
+++ backend/models/db.js
@@ -4,7 +4,7 @@ const pool = new Pool({
   user: 'postgres',
   host: 'localhost',
   database: 'centro_medico',
-  password: 'OctavioySarai2019', // <- contraseña vacía
+  password: 'O4SK5eG6EeT6ka', // <- contraseña vacía
   port: 5432,
 });
 
