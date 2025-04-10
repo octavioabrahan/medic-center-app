1fbb87f6eb38e66137af2b7056d9eb5e51a825a5
Octavio <octavio1709@gmail.com>
2025-04-10 17:45:07 -0400

    modified:   backend/models/profesionalesModel.js

--- backend/models/profesionalesModel.js
+++ backend/models/profesionalesModel.js
@@ -11,12 +11,19 @@ const ProfesionalesModel = {
     return result.rows[0].profesional_id;
   },
   
-
   listar: async () => {
     const result = await db.query(`
-      SELECT p.*, e.nombre AS especialidad
+      SELECT 
+        p.profesional_id,
+        p.cedula,
+        p.nombre,
+        p.apellido,
+        e.nombre AS nombre_especialidad,
+        r.nombre_rol AS nombre_rol
       FROM profesionales p
-      JOIN especialidades e ON p.especialidad_id = e.especialidad_id
+      LEFT JOIN especialidades e ON p.especialidad_id = e.especialidad_id
+      LEFT JOIN roles r ON p.id_rol = r.id_rol
+      ORDER BY p.nombre, p.apellido
     `);
     return result.rows;
   }
