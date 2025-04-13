4dd968df79e5916e563290fa8377b095a1184f7e
Octavio <octavio1709@gmail.com>
2025-04-13 14:06:31 -0400

    modified:   frontend/src/components/public/AgendamientoIndex.js

--- frontend/src/components/public/AgendamientoIndex.js
+++ frontend/src/components/public/AgendamientoIndex.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import './Agendamiento.css';
+import './AgendamientoIndex.css';
 import logo from '../../assets/logo_header.png';
 
 const AgendamientoIndex = () => {
