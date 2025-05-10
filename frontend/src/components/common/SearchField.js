63424559cfa633cfae136e87209ef381581b9316
Octavio <octavio1709@gmail.com>
2025-05-10 19:43:22 -0400

    modified:   frontend/src/components/common/SearchField.js 	modified:   frontend/src/styles/components/_search.css

--- frontend/src/components/common/SearchField.js
+++ frontend/src/components/common/SearchField.js
@@ -62,7 +62,7 @@ const SearchField = ({
     <div className={`search-container ${withButton ? 'search-with-button' : ''}`}>
       <div className={searchClasses}>
         <div className="search-icon-container">
-          <svg className="heroicons-micro-magnifying-glass" width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <svg className="heroicons-micro-magnifying-glass" width="14" height="14" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M21 21L15 15M17 10C17 13.866 13.866 17 10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10Z" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
           </svg>
         </div>
@@ -79,8 +79,8 @@ const SearchField = ({
           <div className="clear-icon-container" onClick={handleClear}>
             <svg
               className="heroicons-micro-x-mark"
-              width="16"
-              height="16"
+              width="14"
+              height="14"
               viewBox="0 0 24 24"
               fill="none"
               xmlns="http://www.w3.org/2000/svg"
