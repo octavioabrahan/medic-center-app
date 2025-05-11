034672721913d511986dd7cfe4c05b3fb540e3f5
Octavio <octavio1709@gmail.com>
2025-05-11 00:32:30 -0400

    modified:   frontend/src/components/admin/CotizacionesAdmin.js

--- frontend/src/components/admin/CotizacionesAdmin.js
+++ frontend/src/components/admin/CotizacionesAdmin.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import './CotizacionesAdmin.css';
-import '../admin/AdminCommon.css'; // Importamos los estilos comunes
 import AdminFilterBar from '../admin/AdminFilterBar'; // Importamos el componente AdminFilterBar
 
 function CotizacionesAdmin() {
