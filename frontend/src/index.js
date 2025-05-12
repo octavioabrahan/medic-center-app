7e4583d6639c2036f0fbd31cc1db14671252140e
Octavio <octavio1709@gmail.com>
2025-05-11 21:03:43 -0400

    modified:   frontend/src/index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,10 +5,10 @@ import ReactDOM from 'react-dom/client';
 import './styles/design-tokens.css';
 
 // 2. Importa el CSS del demo de botones
-import './components/ButtonDemo/ButtonDemo.css';
+import './components/Button/ButtonDemo.css';
 
 // 3. Importa ButtonDemo
-import ButtonDemo from './components/ButtonDemo/ButtonDemo';
+import ButtonDemo from './components/Button/ButtonDemo';
 
 // Si prefieres seguir usando tu App principal y añadir el demo dentro,
 // importa App en lugar de ButtonDemo:
