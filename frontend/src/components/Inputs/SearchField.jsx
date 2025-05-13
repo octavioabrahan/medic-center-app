2977591f66656a3ec76bc46119c4fe8e1f132795
Octavio <octavio1709@gmail.com>
2025-05-13 16:29:53 -0400

    modified:   frontend/src/components/Inputs/SearchField.jsx

--- frontend/src/components/Inputs/SearchField.jsx
+++ frontend/src/components/Inputs/SearchField.jsx
@@ -1,6 +1,7 @@
 import React, { useRef } from "react";
 import PropTypes from "prop-types";
 import "./SearchField.css";
+import { MagnifyingGlassIcon } from '@heroicons/react/20/solid';
 
 /**
  * SearchField component for search input with clear button.
@@ -51,10 +52,7 @@ export default function SearchField({
         </button>
       ) : (
         <span className="heroicons-micro-magnifying-glass" aria-hidden="true">
-          <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <circle cx="7.5" cy="7.5" r="5.5" stroke="currentColor" strokeWidth="1.5"/>
-            <path d="M13 13l-2.5-2.5" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round"/>
-          </svg>
+          <MagnifyingGlassIcon width={16} height={16} />
         </span>
       )}
     </div>
