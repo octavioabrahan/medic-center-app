a7d3970600000a82ed3b7faa6046e48a01dd0263
Octavio <octavio1709@gmail.com>
2025-05-15 10:54:34 -0400

    Changes to be committed: 	deleted:    frontend/src/components/admin/AdminFilterBar.js 	deleted:    frontend/src/components/admin/AdminLayout.js 	deleted:    frontend/src/components/admin/AdminSidebar.js 	deleted:    frontend/src/components/admin/AgendamientoAdmin.js 	deleted:    frontend/src/components/admin/CotizacionesAdmin.js 	deleted:    frontend/src/components/admin/EspecialidadForm.js 	deleted:    frontend/src/components/admin/HorarioForm.js 	deleted:    frontend/src/components/admin/PantallasAdminTab.js 	deleted:    frontend/src/components/admin/ProfesionalForm.js 	deleted:    frontend/src/components/admin/ProfesionalesAdmin.js 	deleted:    frontend/src/components/admin/RolForm.js 	deleted:    frontend/src/components/admin/RolesAdminTab.js 	deleted:    frontend/src/components/admin/TipoAtencionForm.js 	deleted:    frontend/src/components/admin/UsuariosAdminTab.js 	deleted:    frontend/src/pages/admin/AdminAgendamientos.js 	deleted:    frontend/src/pages/admin/AdminCitas.js 	deleted:    frontend/src/pages/admin/AdminConvenios.js 	deleted:    frontend/src/pages/admin/AdminDashboard.js 	deleted:    frontend/src/pages/admin/AdminEmpresas.js 	deleted:    frontend/src/pages/admin/AdminExamenes.js 	deleted:    frontend/src/pages/admin/AdministracionPage.js 	deleted:    frontend/src/pages/admin/AgendamientoAdminPage.js 	deleted:    frontend/src/pages/admin/EspecialidadesPage.js 	deleted:    frontend/src/pages/admin/ExcepcionesPage.js 	deleted:    frontend/src/pages/admin/HorariosPage.js 	deleted:    frontend/src/pages/admin/ProfesionalAsignarServicios.js 	deleted:    frontend/src/pages/admin/ProfesionalesPage.js 	deleted:    frontend/src/pages/admin/RolesPage.js 	deleted:    frontend/src/pages/admin/ServiciosPage.js 	deleted:    frontend/src/pages/admin/TipoAtencionPage.js

--- frontend/src/pages/admin/ProfesionalesPage.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-//import ProfesionalesAdmin from "../../components/admin/ProfesionalesAdmin";
-
-const ProfesionalesPage = () => {
-  return <ProfesionalesAdmin />;
-};
-
-export default ProfesionalesPage;
