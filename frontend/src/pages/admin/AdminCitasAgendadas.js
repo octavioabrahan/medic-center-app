f9a8cecbf6c7d969556ff654de1fa1966cd393f7
Octavio <octavio1709@gmail.com>
2025-05-15 14:56:22 -0400

    modified:   frontend/src/pages/admin/AdminCitasAgendadas.css 	modified:   frontend/src/pages/admin/AdminCitasAgendadas.js

--- frontend/src/pages/admin/AdminCitasAgendadas.js
+++ frontend/src/pages/admin/AdminCitasAgendadas.js
@@ -306,15 +306,18 @@ const AdminCitasAgendadas = () => {
           </div>
           
           <div className="admin-citas-agendadas__select-container">
-            <div className="admin-citas-agendadas__date-picker-wrapper" ref={datePickerRef}>
+            <div className="select-field state-default value-type-value" ref={datePickerRef}>
+              <div className="label">Rango de fechas</div>
               <div 
-                className="admin-citas-agendadas__date-input"
+                className="select select-native-wrapper"
                 onClick={() => {
                   setShowDatePicker(!showDatePicker);
                 }}
               >
-                <span>{format(dateRange.from, 'dd/MM/yyyy')} - {format(dateRange.to, 'dd/MM/yyyy')}</span>
-                <span role="img" aria-label="calendar">📅</span>
+                <span className="select-native">{format(dateRange.from, 'dd/MM/yyyy')} - {format(dateRange.to, 'dd/MM/yyyy')}</span>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="heroicons-micro-calendar" width="20" height="20">
+                  <path d="M6.75 2.25A.75.75 0 017.5 3v1.5h9V3A.75.75 0 0118 3v1.5h.75a3 3 0 013 3v11.25a3 3 0 01-3 3H5.25a3 3 0 01-3-3V7.5a3 3 0 013-3H6V3a.75.75 0 01.75-.75zm13.5 9a1.5 1.5 0 00-1.5-1.5H5.25a1.5 1.5 0 00-1.5 1.5v7.5a1.5 1.5 0 001.5 1.5h13.5a1.5 1.5 0 001.5-1.5v-7.5z" />
+                </svg>
               </div>
               
               {showDatePicker && (
