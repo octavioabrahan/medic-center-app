eb3336e205cdb1cc92e37d424eb5fd790a10e320
Octavio <octavio1709@gmail.com>
2025-05-25 17:26:31 -0400

    modified:   backend/utils/pdf.js

--- backend/utils/pdf.js
+++ backend/utils/pdf.js
@@ -58,13 +58,13 @@ function generarPDF(nombrePaciente, resumen) {
       doc.fontSize(24).fillColor(colors.primary).text('Diagnocentro Acarigua', 50, 45);
       
       // Información de la clínica
-      doc.fontSize(9).fillColor(colors.lightText).text([
+      doc.fontSize(10).fillColor(colors.lightText).text([
         'CALLE 30 ENTRE AV. 33 Y 34 LOCAL C.C. ORION N°. 01 Y 02 SECTOR',
         'CENTRO ACARIGUA ESTADO PORTUGUESA',
         'ZONA POSTAL. 3301',
         '0255-9350349 / 0255-9350351',
         '0255-9350347 / 0255-9883236'
-      ].join('\n'), 400, 45, { align: 'right' });
+      ].join('\n'), 250, 45, { align: 'right', width: 300 });
 
       // Línea separadora
       doc.moveTo(50, 105).lineTo(550, 105).strokeColor(colors.borderColor).stroke();
